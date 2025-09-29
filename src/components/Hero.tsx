763885a872b8a63783b11e06afd81c2ddf722e17
Mohamed Ouakani <mohamedouakani@crc-dot1x-nat-10-239-136-65.bu.edu>
2025-09-29 15:55:43 -0400

    test

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,11 +30,11 @@ export function Hero() {
             </a>
           </div>
         </div>
-        <div className="relative flex justify-center md:justify-start">
+        <div className="relative flex justify-center md:justify-end">
           <img 
             src="https://www.dropbox.com/scl/fi/wwo1kb1f7so4scc6jstud/Ouakani_Headshot.jpg?rlkey=ynd4toxam54rkgkhaqyrpic0v&st=1iwgf6td&raw=1"
             alt="Developer workspace"
-            className="rounded-lg shadow-xl max-w-xs w-full h-auto ml-2 md:ml-8"
+            className="rounded-lg shadow-xl max-w-xs w-full h-auto"
           />
         </div>
       </div>
